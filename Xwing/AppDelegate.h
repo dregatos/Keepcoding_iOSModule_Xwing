612923c7161e55b7af23f1d47a87a2b07cf02cd2
David Regatos <david.regatos@gmail.com>
2015-04-06 23:33:31 +0200

    Initial Commit

---
+++ Xwing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Xwing
+//
+//  Created by David Regatos on 06/04/15.
+//  Copyright (c) 2015 DRG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Xwing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Xwing
+//
+//  Created by David Regatos on 06/04/15.
+//  Copyright (c) 2015 DRG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
